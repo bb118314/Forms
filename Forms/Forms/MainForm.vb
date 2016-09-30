7f0928fb24bae7ff300083698d8955a13f29f85b
bb118314 <roflwhy@gmail.com>
2016-09-30 10:42:16 -0500

    Add files via upload

---
+++ Forms/Forms/MainForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class MainForm
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnDisplayForm_Click(sender As Object, e As EventArgs) Handles btnDisplayForm.Click
+        ' Display a messageform
+        Dim frmMessage As New MessageForm
+        ' Modal style
+        frmMessage.ShowDialog()
+    End Sub
+End Class
